0f78f496c2e530e0674d0b196e860dd16458eb6a
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-10-25 06:44:27 -0400

    added testBuilder to TraitDTOTest

--- src/test/java/com/tylerfitzgerald/demo_api/token/traits/TraitDTOTest.java
+++ src/test/java/com/tylerfitzgerald/demo_api/token/traits/TraitDTOTest.java
@@ -1,5 +1,6 @@
 package com.tylerfitzgerald.demo_api.token.traits;
 
+import com.tylerfitzgerald.demo_api.token.traitTypes.TraitTypeDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -54,6 +55,24 @@ public class TraitDTOTest {
         assertThat(traitDTO.getTraitId()).isEqualTo(TRAIT_ID_2);
         assertThat(traitDTO.getTraitTypeId()).isEqualTo(TRAIT_TYPE_ID_2);
         assertThat(traitDTO.getTraitTypeWeightId()).isEqualTo(TRAIT_TYPE_WEIGHT_ID_2);
+    }
+
+    @Test
+    void testBuilder() {
+        TraitDTO.TraitDTOBuilder builder = TraitDTO.
+                builder().
+                id(ID).
+                traitId(TRAIT_ID).
+                traitTypeId(TRAIT_TYPE_ID).
+                traitTypeWeightId(TRAIT_TYPE_WEIGHT_ID);
+        assertThat(builder).isInstanceOf(TraitDTO.TraitDTOBuilder.class);
+        TraitDTO traitDTO = builder.build();
+        assertThat(traitDTO).isInstanceOf(TraitDTO.class);
+        // Assert that getters return value set 1.
+        assertThat(traitDTO.getId()).isEqualTo(ID);
+        assertThat(traitDTO.getTraitId()).isEqualTo(TRAIT_ID);
+        assertThat(traitDTO.getTraitTypeId()).isEqualTo(TRAIT_TYPE_ID);
+        assertThat(traitDTO.getTraitTypeWeightId()).isEqualTo(TRAIT_TYPE_WEIGHT_ID);
     }
 
 }
