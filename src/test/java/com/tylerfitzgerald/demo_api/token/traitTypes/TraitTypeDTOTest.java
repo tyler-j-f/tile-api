1c8ba93bc3b168510d676ebd75d3c41e273057da
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-10-25 06:37:25 -0400

    added TraitTypeDTOTest

---
+++ src/test/java/com/tylerfitzgerald/demo_api/token/traitTypes/TraitTypeDTOTest.java
@@ -0,0 +1,59 @@
+package com.tylerfitzgerald.demo_api.token.traitTypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class TraitTypeDTOTest {
+
+    // Value set 1
+    private final static Long ID = 1L;
+    private final static Long TRAIT_TYPE_ID = 2L;
+    private final static String TRAIT_TYPE_NAME = "STRING_A";
+    private final static String DESCRIPTION = "STRING_B";
+    // Value set 2
+    private final static Long ID_2 = 3L;
+    private final static Long TRAIT_TYPE_ID_2 = 4L;
+    private final static String TRAIT_TYPE_NAME_2 = "STRING_A";
+    private final static String DESCRIPTION_2 = "STRING_B";
+
+    @Test
+    void testConstructor() {
+        // Create with value set 1.
+        TraitTypeDTO traitTypeDTO = new TraitTypeDTO(
+                ID,
+                TRAIT_TYPE_ID,
+                TRAIT_TYPE_NAME,
+                DESCRIPTION
+        );
+        // Assert that getters return value set 1.
+        assertThat(traitTypeDTO.getId()).isEqualTo(ID);
+        assertThat(traitTypeDTO.getTraitTypeId()).isEqualTo(TRAIT_TYPE_ID);
+        assertThat(traitTypeDTO.getTraitTypeName()).isEqualTo(TRAIT_TYPE_NAME);
+        assertThat(traitTypeDTO.getDescription()).isEqualTo(DESCRIPTION);
+    }
+
+    @Test
+    void testGetterSetters() {
+        // Create with value set 1 initially.
+        TraitTypeDTO traitTypeDTO = new TraitTypeDTO(
+                ID,
+                TRAIT_TYPE_ID,
+                TRAIT_TYPE_NAME,
+                DESCRIPTION
+        );
+        // Set value set 2.
+        traitTypeDTO.setId(ID_2);
+        traitTypeDTO.setTraitTypeId(TRAIT_TYPE_ID_2);
+        traitTypeDTO.setTraitTypeName(TRAIT_TYPE_NAME_2);
+        traitTypeDTO.setDescription(DESCRIPTION_2);
+        // Assert that getters return value set 2.
+        assertThat(traitTypeDTO.getId()).isEqualTo(ID_2);
+        assertThat(traitTypeDTO.getTraitTypeId()).isEqualTo(TRAIT_TYPE_ID_2);
+        assertThat(traitTypeDTO.getTraitTypeName()).isEqualTo(TRAIT_TYPE_NAME_2);
+        assertThat(traitTypeDTO.getDescription()).isEqualTo(DESCRIPTION_2);
+    }
+
+}
