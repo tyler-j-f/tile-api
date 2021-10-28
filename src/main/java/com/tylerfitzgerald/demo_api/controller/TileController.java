024f59d09e35ea825673fd9ccf3d524f2a6fd256
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-10-28 09:23:21 -0400

    added a endpoint to create a tile nft

--- src/main/java/com/tylerfitzgerald/demo_api/controller/TileController.java
+++ src/main/java/com/tylerfitzgerald/demo_api/controller/TileController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tylerfitzgerald.demo_api.erc721.token.TokenFacade;
 import com.tylerfitzgerald.demo_api.erc721.token.TokenFacadeDTO;
+import com.tylerfitzgerald.demo_api.erc721.token.TokenInitializer;
 import com.tylerfitzgerald.demo_api.erc721.token.TokenRetriever;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +18,26 @@ public class TileController extends BaseController {
 
   @Autowired private TokenRetriever tokenRetriever;
 
+  @Autowired private TokenInitializer tokenInitializer;
+
+  @GetMapping("create/{tokenId}")
+  public String createTileNFT(@PathVariable Long tokenId) throws JsonProcessingException {
+    TokenFacadeDTO nft = tokenInitializer.initialize(tokenId);
+    if (nft == null) {
+      System.out.println(
+          "\nDEBUG: nftInitializer->initialize failed. tokenId: " + tokenId.toString());
+      return "Could not create tokenId: " + tokenId;
+    }
+    return new ObjectMapper().writeValueAsString(new TokenFacade(nft).buildTokenDataDTO());
+  }
+
   @GetMapping("get/{tokenId}")
   public String getTileJSON(@PathVariable Long tokenId) throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
     TokenFacadeDTO nft = tokenRetriever.get(tokenId);
     if (nft == null) {
       System.out.println("\nDEBUG: nftInitializer->get failed. tokenId: " + tokenId.toString());
       return "Could not find tokenId: " + tokenId;
     }
-    return objectMapper.writeValueAsString(new TokenFacade(nft).buildTokenDataDTO());
+    return new ObjectMapper().writeValueAsString(new TokenFacade(nft).buildTokenDataDTO());
   }
 }
