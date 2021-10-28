7cbd9f1691e41fa2e4ef7c2e3a81ee90ac6cfad5
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-10-28 11:28:49 -0400

    BaseController changes

--- src/main/java/com/tylerfitzgerald/demo_api/controller/BaseController.java
+++ src/main/java/com/tylerfitzgerald/demo_api/controller/BaseController.java
@@ -9,6 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 public abstract class BaseController {
   @ExceptionHandler(Exception.class)
   public String handleException(HttpServletRequest request, Exception e) {
-    return e.toString();
+    e.printStackTrace();
+    return e.toString() + "\nStack Trace: " + getFormattedStackTrace(e);
+  }
+
+  private String getFormattedStackTrace(Exception e) {
+    String out = "";
+    for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+      out = out + "\n" + stackTraceElement.toString();
+    }
+    return out;
   }
 }
