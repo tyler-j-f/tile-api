4a3f2e29d277c7f161bdcbe9b8a13e115cfff240
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-10-28 17:38:20 -0400

    added metadata YML config files

---
+++ src/main/java/com/tylerfitzgerald/demo_api/config/ContractConfig.java
@@ -0,0 +1,21 @@
+package com.tylerfitzgerald.demo_api.config;
+
+import com.tylerfitzgerald.demo_api.sql.tblTraitTypeWeights.TraitTypeWeightDTO;
+import com.tylerfitzgerald.demo_api.sql.tblTraitTypes.TraitTypeDTO;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.application.contract-config")
+@Data
+public class ContractConfig {
+  private String name;
+  private String description;
+  private String image;
+  private String external_link;
+  private String seller_fee_basis_points;
+  private String background_color;
+  private String animation_url;
+  private String youtube_url;
+}
---
+++ src/main/java/com/tylerfitzgerald/demo_api/config/TokenConfig.java
@@ -0,0 +1,20 @@
+package com.tylerfitzgerald.demo_api.config;
+
+import com.tylerfitzgerald.demo_api.sql.tblTraitTypeWeights.TraitTypeWeightDTO;
+import com.tylerfitzgerald.demo_api.sql.tblTraitTypes.TraitTypeDTO;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.application.token-config")
+@Data
+public class TokenConfig {
+  private String name;
+  private String description;
+  private String image;
+  private String external_url;
+  private String background_color;
+  private String animation_url;
+  private String youtube_url;
+}
