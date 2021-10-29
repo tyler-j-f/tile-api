6644b4404b98aefd46852ebcb88949b13828bd05
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-10-29 14:13:07 -0400

    can find gcloud buckets when ran locally

--- src/main/java/com/tylerfitzgerald/demo_api/controller/TestController.java
+++ src/main/java/com/tylerfitzgerald/demo_api/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.tylerfitzgerald.demo_api.controller;
 
+import com.google.api.gax.paging.Page;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
@@ -17,40 +18,57 @@ public class TestController extends BaseController {
 
   @Autowired private ContractConfig contractConfig;
 
-  @GetMapping("one")
+  //  @GetMapping("one")
+  //  public String one() {
+  //    // The ID of your GCP project
+  //    String projectId = "dev-eth-api";
+  //
+  //    // The ID to give your GCS bucket
+  //    String bucketName = "dev-eth-bucket-1";
+  //
+  //    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+  //
+  //    // See the StorageClass documentation for other valid storage classes:
+  //    //
+  // https://googleapis.dev/java/google-cloud-clients/latest/com/google/cloud/storage/StorageClass.html
+  //    StorageClass storageClass = StorageClass.STANDARD;
+  //
+  //    // See this documentation for other valid locations:
+  //    // http://g.co/cloud/storage/docs/bucket-locations#location-mr
+  //    String location = "US";
+  //
+  //    Bucket bucket =
+  //        storage.create(
+  //            BucketInfo.newBuilder(bucketName)
+  //                .setStorageClass(storageClass)
+  //                .setLocation(location)
+  //                .build());
+  //
+  //    String output =
+  //        "Created bucket "
+  //            + bucket.getName()
+  //            + " in "
+  //            + bucket.getLocation()
+  //            + " with storage class "
+  //            + bucket.getStorageClass();
+  //
+  //    System.out.println(output);
+  //    return output;
+  //  }
+
+  @GetMapping("two")
   public String getContractJSON() {
-    // The ID of your GCP project
-    String projectId = "dev-eth-api";
+    // If you don't specify credentials when constructing the client, the client library will
+    // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+    Storage storage = StorageOptions.getDefaultInstance().getService();
 
-    // The ID to give your GCS bucket
-    String bucketName = "dev-eth-bucket-1";
-
-    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-
-    // See the StorageClass documentation for other valid storage classes:
-    // https://googleapis.dev/java/google-cloud-clients/latest/com/google/cloud/storage/StorageClass.html
-    StorageClass storageClass = StorageClass.STANDARD;
-
-    // See this documentation for other valid locations:
-    // http://g.co/cloud/storage/docs/bucket-locations#location-mr
-    String location = "US";
-
-    Bucket bucket =
-        storage.create(
-            BucketInfo.newBuilder(bucketName)
-                .setStorageClass(storageClass)
-                .setLocation(location)
-                .build());
-
-    String output =
-        "Created bucket "
-            + bucket.getName()
-            + " in "
-            + bucket.getLocation()
-            + " with storage class "
-            + bucket.getStorageClass();
-
-    System.out.println(output);
+    System.out.println("Buckets:");
+    Page<Bucket> buckets = storage.list();
+    String output = "";
+    for (Bucket bucket : buckets.iterateAll()) {
+      output = output + "\n" + bucket.toString();
+      System.out.println(bucket.toString());
+    }
     return output;
   }
 }
--- src/main/java/com/tylerfitzgerald/demo_api/scheduler/Scheduler.java
+++ src/main/java/com/tylerfitzgerald/demo_api/scheduler/Scheduler.java
@@ -1,21 +1,7 @@
 package com.tylerfitzgerald.demo_api.scheduler;
 
-import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.BucketInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageClass;
-import com.google.common.collect.Lists;
-import com.tylerfitzgerald.demo_api.config.ContractConfig;
-import com.tylerfitzgerald.demo_api.config.SalesConfig;
-import com.tylerfitzgerald.demo_api.config.TokenConfig;
-import com.google.cloud.storage.StorageOptions;
 import com.tylerfitzgerald.demo_api.scheduler.tasks.HandleMintEvents;
-import java.io.FileInputStream;
-import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutionException;
@@ -36,38 +22,4 @@ public class Scheduler {
   public void executeTasks() throws ExecutionException, InterruptedException {
     handleMintEventsAndCreateDBTokensTask.execute();
   }
-
-  //  @Scheduled(fixedRateString = "${spring.application.schedulerFixedRateMs}")
-  //  static void authExplicit() throws IOException {
-  //    // You can specify a credential file by providing a path to GoogleCredentials.
-  //    // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment
-  //    // variable.
-  //    GoogleCredentials credentials =
-  //        GoogleCredentials.fromStream(
-  //                new FileInputStream(
-  //                    "src/main/resources/service-account-1-dev-eth-api-key-file.json"))
-  //            .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-  //    Storage storage =
-  // StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-  //
-  //    System.out.println("Buckets:");
-  //    Page<Bucket> buckets = storage.list();
-  //    for (Bucket bucket : buckets.iterateAll()) {
-  //      System.out.println(bucket.toString());
-  //    }
-  //  }
-
-  //  @Scheduled(fixedRateString = "${spring.application.schedulerFixedRateMs}")
-  //  static void authImplicit() {
-  //    // If you don't specify credentials when constructing the client, the client library will
-  //    // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
-  //    Storage storage = StorageOptions.getDefaultInstance().getService();
-  //
-  //    System.out.println("Buckets:");
-  //    Page<Bucket> buckets = storage.list();
-  //    for (Bucket bucket : buckets.iterateAll()) {
-  //      System.out.println(bucket.toString());
-  //    }
-  //  }
-
 }
