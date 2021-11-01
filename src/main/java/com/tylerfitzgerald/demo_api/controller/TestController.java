8f1e6f83c6f820eafea320ad4cdedbdd066b9297
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-01 09:04:51 -0400

    authExplicit works locally

--- src/main/java/com/tylerfitzgerald/demo_api/controller/TestController.java
+++ src/main/java/com/tylerfitzgerald/demo_api/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.tylerfitzgerald.demo_api.controller;
 
 import com.google.api.gax.paging.Page;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageClass;
 import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.Lists;
 import com.tylerfitzgerald.demo_api.config.ContractConfig;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -62,6 +71,29 @@ public class TestController extends BaseController {
   //    return output;
   //  }
 
+  @GetMapping("auth")
+  public String authExplicit() throws IOException {
+    String out = "";
+    Resource resource = new ClassPathResource("classpath:dev-eth-api-d91a5dc6df11.json");
+    InputStream inputStream = resource.getInputStream();
+    // You can specify a credential file by providing a path to GoogleCredentials.
+    // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+    String jsonPath = "classpath:dev-eth-api-d91a5dc6df11.json";
+    GoogleCredentials credentials =
+        GoogleCredentials.fromStream(inputStream)
+            .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+    Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+    System.out.println("Buckets:");
+    out = out + "Buckets:";
+    Page<Bucket> buckets = storage.list();
+    for (Bucket bucket : buckets.iterateAll()) {
+      out = out + "\n" + bucket.toString();
+      System.out.println(bucket.toString());
+    }
+    return out;
+  }
+
   @GetMapping("two")
   public String getContractJSON() {
     // If you don't specify credentials when constructing the client, the client library will
@@ -80,6 +112,21 @@ public class TestController extends BaseController {
 
   @GetMapping("three")
   public String three() throws IOException {
+    String out = "";
+    Resource resource = new ClassPathResource("classpath:dev-eth-api-d91a5dc6df11.json");
+    InputStream inputStream = resource.getInputStream();
+    try {
+      byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
+      String data = new String(bdata, StandardCharsets.UTF_8);
+      out = data.toString();
+    } catch (IOException e) {
+      out = e.toString();
+    }
+    return out;
+  }
+
+  @GetMapping("four")
+  public String four() throws IOException, URISyntaxException {
     String out = "";
     Resource resource = new ClassPathResource("classpath:dev-eth-api-d91a5dc6df11.json");
     InputStream inputStream = resource.getInputStream();
