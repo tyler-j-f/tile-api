74671d9584cf01454c81c76edd437859bff1e99d
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-02 15:17:14 -0400

    image controllers return jpegs

--- src/main/java/com/tylerfitzgerald/demo_api/controller/ImageController.java
+++ src/main/java/com/tylerfitzgerald/demo_api/controller/ImageController.java
@@ -1,9 +1,5 @@
 package com.tylerfitzgerald.demo_api.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tylerfitzgerald.demo_api.erc721.token.TokenFacade;
-import com.tylerfitzgerald.demo_api.erc721.token.TokenFacadeDTO;
 import com.tylerfitzgerald.demo_api.erc721.token.TokenInitializer;
 import com.tylerfitzgerald.demo_api.erc721.token.TokenRetriever;
 import java.io.IOException;
@@ -11,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,35 +17,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = {"/api/image"})
 public class ImageController extends BaseController {
 
-  @Autowired private TokenRetriever tokenRetriever;
-
-  @Autowired private TokenInitializer tokenInitializer;
-
-  @GetMapping("token/create/{tokenId}")
-  public String createTokenImage(@PathVariable Long tokenId) throws JsonProcessingException {
-    return "createTokenImage";
+  @GetMapping(value = "token/create/{tokenId}", produces = MediaType.IMAGE_PNG_VALUE)
+  public void createTokenImage(HttpServletResponse response, @PathVariable Long tokenId)
+      throws IOException {
+    writeImageToOutput(response);
+    return;
   }
 
-  @GetMapping("token/get/{tokenId}")
-  public String getTokenImage(@PathVariable Long tokenId) throws JsonProcessingException {
-    return "getTokenImage";
+  @GetMapping(value = "token/get/{tokenId}", produces = MediaType.IMAGE_PNG_VALUE)
+  public void getTokenImage(HttpServletResponse response, @PathVariable Long tokenId)
+      throws IOException {
+    writeImageToOutput(response);
+    return;
   }
 
-  @GetMapping(value = "test", produces = MediaType.IMAGE_PNG_VALUE)
-  public void getImage(HttpServletResponse response) throws IOException {
+  @GetMapping(value = "contractImage/get/{contractImageId}", produces = MediaType.IMAGE_PNG_VALUE)
+  public void getContractImage(HttpServletResponse response, @PathVariable Long contractImageId)
+      throws IOException {
+    writeImageToOutput(response);
+    return;
+  }
+
+  @GetMapping(value = "saleImage/get/{saleImageId}", produces = MediaType.IMAGE_PNG_VALUE)
+  public void getSaleImage(HttpServletResponse response, @PathVariable Long saleImageId)
+      throws IOException {
+    writeImageToOutput(response);
+    return;
+  }
+
+  private void writeImageToOutput(HttpServletResponse response) throws IOException {
     ClassPathResource imgFile = new ClassPathResource("images/one.jpeg");
     response.setContentType(MediaType.IMAGE_JPEG_VALUE);
     StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
-  }
-
-  @GetMapping("contractImage/get/{contractImageId}")
-  public String getContractImage(@PathVariable Long contractImageId)
-      throws JsonProcessingException {
-    return "getContractImage";
-  }
-
-  @GetMapping("saleImage/get/{saleImageId}")
-  public String getSaleImage(@PathVariable Long saleImageId) throws JsonProcessingException {
-    return "getSaleImage";
   }
 }
