ddc315cddffa9104ee8c2a8dddb94bcbf157d0ea
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-03 19:41:45 -0400

    can call nu.pattern.OpenCV.loadLocally()

--- src/main/java/com/tylerfitzgerald/demo_api/TileApiApplication.java
+++ src/main/java/com/tylerfitzgerald/demo_api/TileApiApplication.java
@@ -1,5 +1,6 @@
 package com.tylerfitzgerald.demo_api;
 
+import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TileApiApplication {
 
   public static void main(String[] args) {
+    nu.pattern.OpenCV.loadLocally();
     SpringApplication.run(TileApiApplication.class, args);
   }
 }
