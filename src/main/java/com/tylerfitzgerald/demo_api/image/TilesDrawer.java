a6a7e381c35abcf27715e1f0cab625fe2b55fbe8
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-11 09:26:21 -0500

    border looks nicE

--- src/main/java/com/tylerfitzgerald/demo_api/image/TilesDrawer.java
+++ src/main/java/com/tylerfitzgerald/demo_api/image/TilesDrawer.java
@@ -11,20 +11,23 @@ public class TilesDrawer {
 
   public Mat drawTiles(Long tokenId) {
     Mat src = new Mat(350, 350, CvType.CV_8UC4);
+    // Set background white
     src.setTo(new Scalar(255, 255, 255, 0));
     // Top left square, blue
     Imgproc.rectangle(src, new Point(0, 38), new Point(175, 175), new Scalar(255, 0, 0, 255), -1);
-    Imgproc.rectangle(src, new Point(0, 38), new Point(175, 175), new Scalar(0, 0, 0, 255), 3);
+    Imgproc.rectangle(src, new Point(0, 38), new Point(175, 175), new Scalar(0, 0, 0, 255), 1);
     // Top right square, green
     Imgproc.rectangle(src, new Point(175, 38), new Point(350, 175), new Scalar(0, 102, 0, 255), -1);
-    Imgproc.rectangle(src, new Point(175, 38), new Point(350, 175), new Scalar(0, 0, 0, 255), 3);
+    Imgproc.rectangle(src, new Point(175, 38), new Point(350, 175), new Scalar(0, 0, 0, 255), 1);
     //    // Bottom left square, red
     Imgproc.rectangle(src, new Point(0, 175), new Point(175, 312), new Scalar(0, 0, 255, 255), -1);
-    Imgproc.rectangle(src, new Point(0, 175), new Point(175, 312), new Scalar(0, 0, 0, 255), 3);
+    Imgproc.rectangle(src, new Point(0, 175), new Point(175, 312), new Scalar(0, 0, 0, 255), 1);
     //    // Bottom right square, yellow
     Imgproc.rectangle(
         src, new Point(175, 175), new Point(350, 312), new Scalar(102, 255, 255, 255), -1);
-    Imgproc.rectangle(src, new Point(175, 175), new Point(350, 312), new Scalar(0, 0, 0, 255), 3);
+    Imgproc.rectangle(src, new Point(175, 175), new Point(350, 312), new Scalar(0, 0, 0, 255), 1);
+    // Draw a border
+    Imgproc.rectangle(src, new Point(0, 0), new Point(349, 349), new Scalar(0, 0, 0, 255), 1);
     //    // Draw title
     Imgproc.putText(
         src,
