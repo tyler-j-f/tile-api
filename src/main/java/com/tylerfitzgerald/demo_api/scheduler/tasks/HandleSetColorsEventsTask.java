6ac1e4a8e5fb9e1960f5c624b61689954ff2d976
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-23 11:20:29 -0500

    reaing pixel values, but reading the preceding zeros

--- src/main/java/com/tylerfitzgerald/demo_api/scheduler/tasks/HandleSetColorsEventsTask.java
+++ src/main/java/com/tylerfitzgerald/demo_api/scheduler/tasks/HandleSetColorsEventsTask.java
@@ -6,13 +6,15 @@ import com.tylerfitzgerald.demo_api.solidityEvents.AbstractEvent;
 import com.tylerfitzgerald.demo_api.solidityEvents.EventRetriever;
 import com.tylerfitzgerald.demo_api.solidityEvents.SetColorsEvent;
 import com.tylerfitzgerald.demo_api.solidityEvents.SolidityEventException;
+import com.tylerfitzgerald.demo_api.sql.tblTraits.TraitsTable;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class HandleSetColorsEventsTask implements TaskInterface {
+  private static final int NUMBER_OF_SUB_TILES = 4;
+  private static final int NUMBER_OF_PIXEL_VALUES = 3;
 
   @Autowired private EventRetriever eventRetriever;
 
@@ -27,7 +29,45 @@ public class HandleSetColorsEventsTask implements TaskInterface {
     List<SetColorsEvent> events =
         getSetColorsEvents(new BigInteger(eventsConfig.getSchedulerNumberOfBlocksToLookBack()));
     System.out.println("DEBUG set colors events: " + events);
+    updateTraitValues(events);
     return;
+  }
+
+  private List<List<String>> getTilesRGBValues(SetColorsEvent event) {
+    List<List<String>> tilesValuesList = new ArrayList<>();
+    for (int x = 0; x < NUMBER_OF_SUB_TILES; x++) {
+      List<String> tileValuesList = getTileRGBValues(x, event.getColors());
+      tilesValuesList.add(tileValuesList);
+    }
+    return tilesValuesList;
+  }
+
+  private List<String> getTileRGBValues(int index, String eventColorsValue) {
+    List<String> tileValuesList = new ArrayList<>();
+    for (int x = 0; x < NUMBER_OF_PIXEL_VALUES; x++) {
+      tileValuesList.add(eventColorsValue.substring(getBeginIndex(x, 0), getEndIndex(x, 0)));
+      tileValuesList.add(eventColorsValue.substring(getBeginIndex(x, 1), getEndIndex(x, 1)));
+      tileValuesList.add(eventColorsValue.substring(getBeginIndex(x, 2), getEndIndex(x, 2)));
+    }
+    return tileValuesList;
+  }
+
+  private int getBeginIndex(int tileIndex, int pixelIndex) {
+    return (tileIndex * 9) + (pixelIndex * 3);
+  }
+
+  private int getEndIndex(int tileIndex, int pixelIndex) {
+    return getBeginIndex(tileIndex, pixelIndex) + 3;
+  }
+
+  private void updateTraitValue(SetColorsEvent event) {
+    System.out.println("DEBUG one event RGB values: " + getTilesRGBValues(event));
+  }
+
+  private void updateTraitValues(List<SetColorsEvent> events) {
+    for (SetColorsEvent event : events) {
+      updateTraitValue(event);
+    }
   }
 
   private List<SetColorsEvent> getSetColorsEvents(BigInteger numberOfBlocksAgo)
