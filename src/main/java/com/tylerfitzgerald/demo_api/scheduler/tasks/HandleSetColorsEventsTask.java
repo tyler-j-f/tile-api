79297f379a912b47dfcf9bda6f29c6ea3ee9dea7
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-23 15:38:14 -0500

    finding the trait to update, now would jsut need to save the value

--- src/main/java/com/tylerfitzgerald/demo_api/scheduler/tasks/HandleSetColorsEventsTask.java
+++ src/main/java/com/tylerfitzgerald/demo_api/scheduler/tasks/HandleSetColorsEventsTask.java
@@ -1,12 +1,14 @@
 package com.tylerfitzgerald.demo_api.scheduler.tasks;
 
 import com.tylerfitzgerald.demo_api.config.EventsConfig;
-import com.tylerfitzgerald.demo_api.erc721.token.TokenInitializeException;
-import com.tylerfitzgerald.demo_api.solidityEvents.AbstractEvent;
+import com.tylerfitzgerald.demo_api.erc721.token.TokenFacadeDTO;
+import com.tylerfitzgerald.demo_api.erc721.token.TokenRetriever;
+import com.tylerfitzgerald.demo_api.erc721.traits.WeightlessTraitTypeConstants;
+import com.tylerfitzgerald.demo_api.image.ImageException;
 import com.tylerfitzgerald.demo_api.solidityEvents.EventRetriever;
 import com.tylerfitzgerald.demo_api.solidityEvents.SetColorsEvent;
 import com.tylerfitzgerald.demo_api.solidityEvents.SolidityEventException;
-import com.tylerfitzgerald.demo_api.sql.tblTraits.TraitsTable;
+import com.tylerfitzgerald.demo_api.sql.tblWeightlessTraits.WeightlessTraitDTO;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +19,8 @@ public class HandleSetColorsEventsTask implements TaskInterface {
   private static final int NUMBER_OF_PIXEL_VALUES = 3;
 
   @Autowired private EventRetriever eventRetriever;
-
   @Autowired private EventsConfig eventsConfig;
+  @Autowired private TokenRetriever tokenRetriever;
 
   @Override
   public void execute() throws SolidityEventException {
@@ -29,7 +31,7 @@ public class HandleSetColorsEventsTask implements TaskInterface {
     List<SetColorsEvent> events =
         getSetColorsEvents(new BigInteger(eventsConfig.getSchedulerNumberOfBlocksToLookBack()));
     System.out.println("DEBUG set colors events: " + events);
-    updateTraitValues(events);
+    updateTraitValuesForEvents(events);
     return;
   }
 
@@ -60,13 +62,48 @@ public class HandleSetColorsEventsTask implements TaskInterface {
     return getBeginIndex(tileIndex, pixelIndex) + 3;
   }
 
-  private void updateTraitValue(SetColorsEvent event) {
-    System.out.println("DEBUG one event RGB values: " + getTilesRGBValues(event));
+  private void updateTraitValuesForEvent(SetColorsEvent event, TokenFacadeDTO nft) {
+    int tileIndex = 0;
+    List<WeightlessTraitDTO> traits = nft.getWeightlessTraits();
+    WeightlessTraitDTO trait;
+    for (List<String> tileRGBValues : getTilesRGBValues(event)) {
+      switch (tileIndex) {
+        case 0:
+          trait =
+              traits.stream()
+                  .filter(
+                      weightlessTraitDTO ->
+                          weightlessTraitDTO
+                              .getTraitTypeId()
+                              .equals(Long.valueOf(WeightlessTraitTypeConstants.TILE_1_COLOR)))
+                  .findFirst()
+                  .get();
+          System.out.println("DEBUG: ORIGINAL TRAIT: " + trait);
+          trait.setValue(tileRGBValues.get(0) + tileRGBValues.get(1) + tileRGBValues.get(2));
+          System.out.println("DEBUG: UPDATED TRAIT: " + trait);
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        default:
+          break;
+      }
+      tileIndex++;
+    }
   }
 
-  private void updateTraitValues(List<SetColorsEvent> events) {
+  private void updateTraitValuesForEvents(List<SetColorsEvent> events) {
     for (SetColorsEvent event : events) {
-      updateTraitValue(event);
+      TokenFacadeDTO nft =
+          tokenRetriever.get(Long.valueOf(strip0xFromHexString(event.getTokenId())));
+      if (nft == null) {
+        System.out.println("ERROR!!! This token does not exist");
+        continue;
+      }
+      updateTraitValuesForEvent(event, nft);
     }
   }
 
