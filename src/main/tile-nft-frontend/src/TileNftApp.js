5d37e0381d0c8767fb6b81a19b63a32e204de3f4
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-17 15:10:48 -0500

    added my own title

---
+++ src/main/tile-nft-frontend/src/TileNftApp.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TileNftHeader from "./TileNftHeader";
+
+function TileNftApp() {
+  return (
+    <div className="App">
+      <TileNftHeader />
+    </div>
+  );
+}
+
+export default TileNftApp;
