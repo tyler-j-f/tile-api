09a23483780a98ab65c1f147f3318f850fabe9d8
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-19 14:07:08 -0500

    added a nav and a menu

--- src/main/tile-nft-frontend/src/TileNftApp.js
+++ src/main/tile-nft-frontend/src/TileNftApp.js
@@ -1,11 +1,59 @@
 import './css/App.css';
 import TileNftHeader from "./header/TileNftHeader";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// components
+import Toggle from './components/Toggle';
+import Menu from './components/Menu';
+// pages
+import HomePage from './pages/HomePage';
+import ExamplePage from './pages/ExamplePage';
+import ContactPage from './pages/ContactPage'
+
+// function TileNftApp() {
+//   return (
+//     <div className="App">
+//       <TileNftHeader />
+//     </div>
+//   );
+// }
+//
+// export default TileNftApp;
 
 function TileNftApp() {
+
+  const [navToggled, setNavToggled] = useState(false);
+
+  const handleNavToggle = () => {
+    setNavToggled(!navToggled);
+  }
+
+  // return (
+  //     <div className="App">
+  //       <Toggle handleNavToggle={handleNavToggle}/>
+  //       <Router>
+  //         { navToggled ? <Menu handleNavToggle={handleNavToggle} /> : null }
+  //         <Switch>
+  //           <Route exact path="/" component={HomePage} />
+  //           <Route exact path="/example" component={ExamplePage} />
+  //           <Route exact path="/contact" component={ContactPage} />
+  //         </Switch>
+  //       </Router>
+  //     </div>
+  // );
+
   return (
-    <div className="App">
-      <TileNftHeader />
-    </div>
+      <div className="App">
+        <Toggle handleNavToggle={handleNavToggle}/>
+        <Router>
+          { navToggled ? <Menu handleNavToggle={handleNavToggle} /> : null }
+          <Routes>
+            <Route exact path="/" element={<HomePage/>}/>
+            <Route exact path="/example" element={<ExamplePage/>}/>
+            <Route exact path="/contact" element={<ContactPage/>}/>
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
--- src/main/tile-nft-frontend/src/components/Toggle.js
+++ src/main/tile-nft-frontend/src/components/Toggle.js
@@ -1,8 +1,27 @@
-function Toggle() {
+import React from 'react'
+import styled from 'styled-components';
+
+import { FaBars } from 'react-icons/fa';
+
+const Toggle = ({handleNavToggle}) => {
   return (
-      <div>
-      </div>
-  );
+      <StyledToggle className="animate__animated animate__fadeInRight" onClick={handleNavToggle}>
+        <FaBars/>
+      </StyledToggle>
+  )
 }
 
-export default Toggle;
+const StyledToggle = styled.button`
+    position: fixed;
+    top: 5%;
+    right: 4%;
+    color: #222;
+    background: #eee;
+    padding: .75rem;
+    display: flex;
+    place-items: center;
+    font-size: 2rem;
+    cursor: pointer;
+`;
+
+export default Toggle
