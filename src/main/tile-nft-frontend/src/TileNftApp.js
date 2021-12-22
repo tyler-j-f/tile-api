31ca87c9f596b51fa8850a496e1a10259128f612
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-22 15:46:10 -0500

    error code stuff works

--- src/main/tile-nft-frontend/src/TileNftApp.js
+++ src/main/tile-nft-frontend/src/TileNftApp.js
@@ -1,7 +1,7 @@
 // style
 import './css/App.css';
 import TileNftHeader from "./header/TileNftHeader";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // components
 import Toggle from './components/Toggle';
@@ -12,14 +12,36 @@ import ViewPage from './pages/ViewPage';
 import ContactPage from './pages/ContactPage'
 import LeaderboardPage from "./pages/LeaderboardPage";
 import {Col, Container, Row} from "react-bootstrap";
+import styled from "styled-components";
 
 function TileNftApp() {
 
   const [navToggled, setNavToggled] = useState(false);
+  const [hasNotFoundError, setHasNotFoundError] = useState(false);
+  const [hasInternalError, setHasInternalError] = useState(false);
 
   const handleNavToggle = () => {
     setNavToggled(!navToggled);
   }
+
+  useEffect(() => {
+    let errorCode = new URL(window.location.href).searchParams.get('errorCode');
+    if (errorCode === null) {
+      setHasNotFoundError(false);
+      setHasInternalError(false);
+      return;
+    }
+    console.log('errorCode: ' + errorCode);
+    if (errorCode === '404' && !hasNotFoundError) {
+      setHasNotFoundError(!hasNotFoundError);
+      return;
+    }
+    if (errorCode === '500' && !hasInternalError) {
+      setHasInternalError(!hasInternalError);
+      return;
+    }
+    return;
+  });
 
   return (
       <div className="App">
@@ -29,6 +51,10 @@ function TileNftApp() {
             <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4} >
               <Toggle handleNavToggle={handleNavToggle}/>
             </Col>
+          </Row>
+          <Row>
+            {hasNotFoundError ? getNotFoundErrorText() : null}
+            {hasInternalError ? getInternalErrorText() : null}
           </Row>
           <Router>
             { navToggled ? <Menu handleNavToggle={handleNavToggle} /> : null }
@@ -43,5 +69,24 @@ function TileNftApp() {
       </div>
   );
 }
+
+const getNotFoundErrorText = () => {
+  return (
+      <StyledErrorText>Error! Page not found. Please check the URL is correct and then retry.</StyledErrorText>
+  )
+}
+
+const getInternalErrorText = () => {
+  return (
+      <StyledErrorText>Internal error! Please try again later.</StyledErrorText>
+  )
+}
+
+const StyledErrorText =
+    styled.p`
+    display: block;
+    color: #FF4500;
+    text-align: center;
+    `;
 
 export default TileNftApp;
