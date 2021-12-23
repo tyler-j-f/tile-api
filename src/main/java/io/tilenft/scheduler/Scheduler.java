f7b10cda92258a9d08bff454bef44cc1784ccd32
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-23 10:35:04 -0500

    Added shouldResetSqlTables to scheduler

--- src/main/java/io/tilenft/scheduler/Scheduler.java
+++ src/main/java/io/tilenft/scheduler/Scheduler.java
@@ -6,6 +6,7 @@ import io.tilenft.scheduler.tasks.HandleSetColorsEventsTask;
 import io.tilenft.scheduler.tasks.HandleSetEmojisEventsTask;
 import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ public class Scheduler {
   @Autowired private HandleSetColorsEventsTask handleSetColorsEventsTask;
   @Autowired private HandleSetEmojisEventsTask handleSetEmojisEventsTask;
   @Autowired private HandleMergeEventsTask handleMergeEventsTask;
+  @Autowired private Environment env;
 
   /**
    * Execute tasks every schedulerFixedRateMs If you would like to execute tasks on a different
@@ -26,9 +28,10 @@ public class Scheduler {
    */
   @Scheduled(fixedRateString = "${spring.application.events-config.schedulerFixedRateMs}")
   public void executeTasks() throws TaskSchedulerException {
+    boolean shouldResetSqlTables = Boolean.parseBoolean(env.getProperty("shouldResetSqlTables"));
     handleMintEventsTask.execute();
-    //    handleSetColorsEventsTask.execute();
-    //    handleSetEmojisEventsTask.execute();
+    handleSetColorsEventsTask.execute();
+    handleSetEmojisEventsTask.execute();
     handleMergeEventsTask.execute();
   }
 }
