eccd40e946aaa7e254de244dffb90bd18d84c8bd
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-28 18:37:30 -0500

    added useDapp

--- src/main/tile-nft-frontend/src/updateTileNft/UpdateTileNft.js
+++ src/main/tile-nft-frontend/src/updateTileNft/UpdateTileNft.js
@@ -1,10 +1,12 @@
 import {Component} from 'react';
 import styled from 'styled-components';
 import Spinner from 'react-bootstrap/Spinner';
+import { Rinkeby } from '@usedapp/core'
 
 class Leaderboard extends Component {
 
   constructor(props) {
+    console.log(Rinkeby);
     super(props);
   }
 
