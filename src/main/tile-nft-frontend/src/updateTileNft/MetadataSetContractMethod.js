5f8a0654086f6bb1483fd8829ff3af5b75b9f44f
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-29 15:47:06 -0500

    Loaded the contract with ethers lib

--- src/main/tile-nft-frontend/src/updateTileNft/MetadataSetContractMethod.js
+++ src/main/tile-nft-frontend/src/updateTileNft/MetadataSetContractMethod.js
@@ -1,57 +1,27 @@
-import {
-  useChainCall, useEtherBalance, useEthers, ERC20
-} from "@usedapp/core";
-import {Interface} from '@ethersproject/abi'
+
 import {Button} from "react-bootstrap";
 import TileContract from '../contractsJson/Tile.json'
+import { ethers } from "ethers";
+import {useEthers} from "@usedapp/core";
+import {useEffect} from "react";
 
 function MetadataSetContractMethod() {
 
-  const {activateBrowserWallet, account } = useEthers();
-  const etherBalance = useEtherBalance(account);
+  const { library } = useEthers()
 
-  function handleConnectWallet() {
-    activateBrowserWallet();
-  }
-
-  function useTokenAllowance(
-      tokenAddress,
-      ownerAddress
-  ) {
-    console.log("DEBUGGING hook");
-    const balance =
-    useChainCall(account && {
-      address: tokenAddress,
-      data: new Interface(TileContract.abi).encodeFunctionData('metadataSet', [1, 0, '0x2550000000001530000000001532552042040000000000000000000000000000'])
-    });
-    console.log("DEBUGGING balance");
-    console.log(balance);
-    return balance
-  }
-
-  function testHandler() {
-    console.log("DEBUGGING");
-  }
-
-  const test = useTokenAllowance(
-      CONTRACT_ADDRESS,
-      OWNER_ADDRESS
-  );
+  useEffect(() => {
+    if (library) {
+      console.log("Account exists.")
+      let contract = new ethers.Contract(CONTRACT_ADDRESS, TileContract.abi, library);
+      console.log(contract);
+    }
+    return;
+  }, []);
 
   return (
       <>
-        <Button onClick={handleConnectWallet}>
-          <p>
-            Login
-          </p>
+        <Button >
         </Button>
-        {account &&
-          <Button onClick={testHandler}>
-            <p>
-              Test send a transaction
-            </p>
-          </Button>
-        }
       </>
   );
 }
