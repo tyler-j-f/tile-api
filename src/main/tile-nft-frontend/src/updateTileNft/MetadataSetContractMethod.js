20f5d9b366bdf8d1f02ac146ab4e8b4630ea402c
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-29 10:48:46 -0500

    getting Invalid contract call, for metadataSet

--- src/main/tile-nft-frontend/src/updateTileNft/MetadataSetContractMethod.js
+++ src/main/tile-nft-frontend/src/updateTileNft/MetadataSetContractMethod.js
@@ -8,36 +8,32 @@ import {Button} from "react-bootstrap";
 
 function MetadataSetContractMethod() {
 
-  function useTokenAllowance(
-      tokenAddress,
-      ownerAddress
+  const tokenIdToUpdate = 4;
+
+  function useMetadataSet(
+      contractAddress
   ) {
-    console.log("DEBUGGING hook");
-    const balance =
+    const callResult =
     useContractCall({
       abi: new Interface(TileContract.abi),
-      address: tokenAddress,
-      method: 'balanceOf',
-      args: [ownerAddress],
+      address: contractAddress,
+      method: CONTRACT_METHOD_NAME,
+      args: [
+        tokenIdToUpdate, COLOR_METADATASET_INDEX, METADATA_TO_SET
+      ]
+      // args: getContractCallArgs({tokenId: tokenIdToUpdate, dataToSetIndex: COLOR_METADATASET_INDEX, rgbValue: METADATA_TO_SET})
     });
-    console.log("DEBUGGING balance");
-    console.log(balance);
-    return balance
+    console.log(callResult);
+    return callResult
   }
 
   function testHandler() {
     console.log("DEBUGGING");
   }
 
-  const test = useTokenAllowance(
-      CONTRACT_ADDRESS,
-      OWNER_ADDRESS
+  useMetadataSet(
+      CONTRACT_ADDRESS
   );
-
-  function getAbiTextFromFile() {
-    return fetch('../../../resources/contractsJson/Tile.json')
-    .then(response => response.text())
-  }
 
   function getContractCallArgs({tokenId, dataToSetIndex, rgbValue}) {
     return [
@@ -54,8 +50,11 @@ function MetadataSetContractMethod() {
   );
 }
 
-export const CONTRACT_ADDRESS     = "0xEc9547ABc4a8c24B99226BeE239c6E29814903Cd";
-export const OWNER_ADDRESS        = "0x4fdF8DF271e1A65B119D858eeA2A7681da8F9c15";
-export const CONTRACT_METHOD_NAME = "metadataSet";
+export const CONTRACT_ADDRESS                      = "0xEc9547ABc4a8c24B99226BeE239c6E29814903Cd";
+export const CURRENT_USER_ADDRESS_ADDRESS          = "0x4fdF8DF271e1A65B119D858eeA2A7681da8F9c15";
+export const CONTRACT_METHOD_NAME                  = "metadataSet";
+export const METADATA_TO_SET                       = "0x1210000550001112221110012220110220330000000000000000000000000000";
+export const COLOR_METADATASET_INDEX               = 0;
+export const EMOJI_METADATASET_INDEX               = 1;
 
 export default MetadataSetContractMethod
