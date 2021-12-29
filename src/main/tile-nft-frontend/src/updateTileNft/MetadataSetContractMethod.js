28d18ba4db5ee0ed025f1629a966d795b39b0346
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-12-29 13:22:09 -0500

    Revert "Added logic to send eth"
    This reverts commit c42b1ecf1c70d07edfb41a43ac72bed1988093a8.

--- src/main/tile-nft-frontend/src/updateTileNft/MetadataSetContractMethod.js
+++ src/main/tile-nft-frontend/src/updateTileNft/MetadataSetContractMethod.js
@@ -1,57 +1,60 @@
 import {
   ERC20Interface,
-  useSendTransaction, useEtherBalance, useEthers
+  useContractCall
 } from "@usedapp/core";
 import TileContract from '../contractsJson/Tile.json'
 import { Interface } from '@ethersproject/abi'
 import {Button} from "react-bootstrap";
 
 function MetadataSetContractMethod() {
-  const {activateBrowserWallet, account } = useEthers();
-  const etherBalance = useEtherBalance(account);
-  const { sendTransaction, state } = useSendTransaction();
 
-  function handleConnectWallet() {
-    activateBrowserWallet();
-  }
-
-  function handleSendTransactionClick(e) {
-    e.preventDefault();
-    // let data = sendTransaction({
-    //   to: '0x273d7D3E390885cc5c68Def05F6D7ef5B78b6D57',
-    //   from: '0x4fdF8DF271e1A65B119D858eeA2A7681da8F9c15',
-    //   from: 'metadataSet',
-    //   args: [1, 0, '0x2550000000001530000000001532552042040000000000000000000000000000'],
-    // });
-    // console.log(data);
-    console.log(sendTransaction);
-    console.log(state);
-    return null;
+  function useTokenAllowance(
+      tokenAddress,
+      ownerAddress
+  ) {
+    console.log("DEBUGGING hook");
+    const balance =
+    useContractCall({
+      abi: new Interface(TileContract.abi),
+      address: tokenAddress,
+      method: 'balanceOf',
+      args: [ownerAddress],
+    });
+    console.log("DEBUGGING balance");
+    console.log(balance);
+    return balance
   }
 
   function testHandler() {
     console.log("DEBUGGING");
   }
 
+  const test = useTokenAllowance(
+      CONTRACT_ADDRESS,
+      OWNER_ADDRESS
+  );
+
+  function getAbiTextFromFile() {
+    return fetch('../../../resources/contractsJson/Tile.json')
+    .then(response => response.text())
+  }
+
+  function getContractCallArgs({tokenId, dataToSetIndex, rgbValue}) {
+    return [
+      tokenId, dataToSetIndex, rgbValue
+    ];
+  }
+
   return (
-      <>
-        <Button onClick={handleConnectWallet}>
-          <p>
-            Login
-          </p>
-        </Button>
-        {account && etherBalance && (
-            <Button onClick={handleSendTransactionClick}>
-              <p>
-                Test send a transaction
-              </p>
-            </Button>
-        )}
-      </>
+      <Button onClick={testHandler}>
+        <p>
+          Test send a transaction
+        </p>
+      </Button>
   );
 }
 
-export const CONTRACT_ADDRESS     = "0x273d7D3E390885cc5c68Def05F6D7ef5B78b6D57";
+export const CONTRACT_ADDRESS     = "0xEc9547ABc4a8c24B99226BeE239c6E29814903Cd";
 export const OWNER_ADDRESS        = "0x4fdF8DF271e1A65B119D858eeA2A7681da8F9c15";
 export const CONTRACT_METHOD_NAME = "metadataSet";
 
--- src/main/tile-nft-frontend/src/updateTileNft/UpdateTileNft.js
+++ src/main/tile-nft-frontend/src/updateTileNft/UpdateTileNft.js
@@ -19,6 +19,7 @@ class Leaderboard extends Component {
   render() {
     return (
         <>
+          <ConnectButton />
           <MetadataSetContractMethod />
         </>
     );
