5016cfa9e899e8e95adc6c272fbcfb4366418d2e
Tyler Fitzgerald <tylerjf1993@gmail.com>
2022-02-02 08:36:53 -0500

    Revert "inbetween thursday night. cannot load vie wtoken page owner of token address"
    This reverts commit de76a29e27987c26603ea848ecb22e7177ccc990.

--- src/main/tile-nft-frontend/src/view/loadViewTokenData.js
+++ src/main/tile-nft-frontend/src/view/loadViewTokenData.js
@@ -1,5 +1,4 @@
 import loadBlockExplorerUrl from "./loadBlockExplorerUrl";
-import loadContractAddress from "./loadContractAddress";
 
 const noop = () => {};
 
@@ -12,6 +11,31 @@ const loadViewTokenData = ({
       return `${window.location.origin}/api/image/tile/get/${tokenId}`;
     }
     return getMetadataToUpdateTokenUrl(tokenId, metadataToUpdate);
+  }
+
+  const loadContractAddress = () => {
+    return fetch(
+        `${window.location.origin}/api/contract/getAddress`,
+        {method: 'get'}
+    )
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return null;
+    })
+    .then(json => {
+      console.log("ViewToken loadContractAddress json found", json);
+      if (json === null) {
+        let errorMessage = 'loadContractAddress json is null';
+        console.log(errorMessage);
+        throw errorMessage;
+      }
+      return json
+    })
+    .catch(err => {
+      console.log("Error caught!!!", err);
+    });
   }
 
   const loadTokenImageData = (tokenId) => {
